1e7076bc646b5f943324bf3270578104a698caa7
Carlos Pacheco <acprd_carlinhos@hotmail.com>
2023-02-24 00:49:52 -0400

    Favorites Screen - Persistence

--- CodeView/Screens/FollowerListVC.swift
+++ CodeView/Screens/FollowerListVC.swift
@@ -47,6 +47,62 @@ class FollowerListVC: UIViewController {
   func configureViewController() {
     navigationController?.navigationBar.prefersLargeTitles = true
     view.backgroundColor = .systemBackground
+
+    let addButton = UIBarButtonItem(
+      barButtonSystemItem: .add,
+      target: self,
+      action: #selector(addButtonTapped)
+    )
+
+    navigationItem.rightBarButtonItem = addButton
+    view.backgroundColor = .systemBackground
+  }
+
+  @objc func addButtonTapped() {
+    showLoadingView()
+
+    NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+      guard let self = self else { return }
+
+      self.dismissLoadingView()
+
+      switch result {
+        case .success(let user):
+          let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+
+          PersistenceManager.updateWith(
+            favorite: favorite,
+            actionType: .add
+          ) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error
+            else {
+              self.presentGFAlertOnMainThread(
+                title: "Success!",
+                message: "You have successfully favorited this user👍",
+                buttonTitle: "Hooray!"
+              )
+
+              return
+            }
+
+            self.presentGFAlertOnMainThread(
+              title: "Something went wrong",
+              message: error.rawValue,
+              buttonTitle: "Ok"
+            )
+
+          }
+
+        case .failure(let error):
+          self.presentGFAlertOnMainThread(
+            title: "something went wrong",
+            message: error.rawValue,
+            buttonTitle: "Ok"
+          )
+      }
+
+    }
   }
 
   func configureCollectionView() {
--- CodeView/Utilities/GFError.swift
+++ CodeView/Utilities/GFError.swift
@@ -8,8 +8,12 @@
 import Foundation
 
 enum GFError: String, Error {
+
   case invalidUsername = "This username created an invalid request. Please try again."
   case unableToComplete = "Unable to complete your request. PLease check your internet connection"
   case invalidResponse = "Invalid response from server. Please try again."
   case invalidData = "The data received from the server was invalid. Please try again."
+  case unableToFavorite = "There was an error favoriting this user. Please Try again."
+  case alreadyInFavorites = "You`ve already favorited this user. You must REALLY like them!"
+
 }
